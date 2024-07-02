8a2b407146b2c0ed734d2cfc0369e1f88e576a6c
Gustavo Mello <gustavomello9178@gmail.com>
2024-07-02 16:29:37 -0300

    feat(code-page): Implementação da fórmula de conversao de graus fah para cels

--- code.js
+++ code.js
@@ -1,1 +1,5 @@
 temperatura = prompt("Digite a temperatura em graus Fahrenheit")
+
+celsius = (temperatura -32) * 5/9 
+
+console.log("O valor correpondente da temperatura de " + temperatura + " graus Fahrenheit em graus Celsius é de " + celsius)
